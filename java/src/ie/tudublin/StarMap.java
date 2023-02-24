1d92d3342b4d55517fe7eefa8f02964c520d8f7f
jiaqianzhang <jiaqianlyrb@gmail.com>
2023-02-24 10:30:07 +0000

    star mapping

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -11,7 +11,7 @@ public class StarMap extends PApplet
 
 	public void setup()
 	{
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
 
 		smooth();
@@ -20,8 +20,8 @@ public class StarMap extends PApplet
 
 	public void drawGrid()
 	{
-		stroke(255);
-		float boarder = 50.0f;
+		stroke(0, 255, 255); // setting the colour aquamerine
+		float boarder = width * 0.1f; // the size of the border will scale depending on the size of the screen (calculatig either the side)
 
 		int count = 10;
 		float gap = (width - (boarder * 2.0f)) / (float)count;
@@ -30,10 +30,22 @@ public class StarMap extends PApplet
 			float x = boarder + (gap * i+5);
 			line(x, boarder, x, height - boarder);
 			line(boarder, x, width - boarder, x);
-
+            textAlign(CENTER,CENTER); // sets the horizontal and vertical line for text
+            text(i,x+(gap *0.5f), gap*0.5f);// 1 parameter is what you want to print
 		}
+        float f = map(5, 0, 10, 100, 200); // takes number 5, minus half way between 0 and 10 and puts the output number halfway between 100 and 200 => 150
+        float f1 = map1(5, 0, 10, 100, 200);
+    }
 
-	}
+    // build in function
+    float map1(float a, float b, float c, float d, float e)
+    {
+        float r1 = c - b;
+        float r2 = e - d;
+        float distance = a - b;
+
+        return d +((distance / r1) * r2);
+    }
 
 	public void draw()
 	{	
